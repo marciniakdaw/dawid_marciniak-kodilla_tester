07f33249b1c6cd92d0d69972ee956afa7f48f2fc
Dawid Marciniak <marciniakdaw@wp.pl>
2021-02-18 16:23:24 +0100

    HalloWorld

---
+++ kodilla_intro/src/main/java/HalloWorld.java
@@ -0,0 +1,8 @@
+public class HalloWorld {
+    public static void main(String[] args){
+        System.out.println("Hallo World!git");
+        System.out.println("Dawid");
+        System.out.println(45);
+        System.out.println("M.");
+    }
+}
