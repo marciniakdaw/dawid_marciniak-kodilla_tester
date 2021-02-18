3d80828af52e6e8e9dcc783d2fb6ab9d13781e9c
Dawid Marciniak <marciniakdaw@wp.pl>
2021-02-18 16:55:44 +0100

    MainExtended

---
+++ kodilla_intro/src/main/java/MainExtended.java
@@ -0,0 +1,19 @@
+public class MainExtended {
+    public static void main(String[] args){
+        String title = "VARIABLES.";
+        String myName = "Dawid";
+        int myAge = 45;
+        double myHight = 1.95;
+        boolean doIlikeMyCar = true;
+        char myFullName = 'M';
+        System.out.println("");
+        System.out.println("To mój prosty program na metamat użycia: " + title);
+        System.out.println("");
+        System.out.println("Witaj! Nazywam się " + myName + " " + myFullName +".");
+        System.out.println("");
+        System.out.println("Mam " + myAge + " lat i " + myHight + " cm wzrostu.");
+        System.out.println("");
+        System.out.println("Czy lubię swój samochód? Odpowiedź może być tylko jedna: " + doIlikeMyCar + ":-)");
+
+    }
+}
