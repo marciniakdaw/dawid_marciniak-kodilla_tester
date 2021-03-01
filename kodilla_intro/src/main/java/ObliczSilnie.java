88e4c05d3a1b036743f10cd40766c52e1d5cbbc6
Dawid Marciniak <marciniakdaw@wp.pl>
2021-03-01 11:56:29 +0100

    Silnia

--- kodilla_intro/src/main/java/ObliczSilnie.java
+++ kodilla_intro/src/main/java/ObliczSilnie.java
@@ -1,2 +1,10 @@
-package PACKAGE_NAME;public class ObliczSilnie {
+public class ObliczSilnie {
+
+    public static void main (String[] args){
+
+        Silnia number1 = new Silnia();
+        number1.setNumber(5);
+        number1.showResult();
+
+    }
 }
--- kodilla_intro/src/main/java/Silnia.java
+++ kodilla_intro/src/main/java/Silnia.java
@@ -1,2 +1,18 @@
-package PACKAGE_NAME;public class Silnia {
+public class Silnia {
+    private int number;
+    private int result;
+
+    public void setNumber(int newNumber){
+        number = newNumber;
+        }
+    public int result(){
+        int result1 = 1;
+        for (int i = 1; i <= number; i++) {
+            result1 = result1 * i;
+        }
+        return result1;
+    }
+    public void showResult(){
+        System.out.println("Silnia z " + number + " wynosi " + result);
+    }
 }
